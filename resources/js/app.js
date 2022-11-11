1fdee96858d6a69564e3b59b11b54ab41b94e622
dj-idfx <dj_idfx@hotmail.com>
2022-11-11 12:40:57 +0100

    Optimize js / css build

--- resources/js/app.js
+++ resources/js/app.js
@@ -13,6 +13,9 @@ window.Popper = Popper;
 
 // Bootstrap JS Option 1: Import separate Bootstrap 5 plugins
 // import { Alert, Button, Carousel, Collapse, Dropdown, Modal, Offcanvas, Popover, ScrollSpy, Tab, Toast, Tooltip } from 'bootstrap';
+import 'bootstrap/js/dist/collapse'
+import 'bootstrap/js/dist/dropdown';
+
 // Bootstrap JS Option 2: Import full Bootstrap 5 JS
-import * as Bootstrap from '~bootstrap';
-window.Bootstrap = Bootstrap;
+// import * as Bootstrap from '~bootstrap';
+// window.Bootstrap = Bootstrap;
